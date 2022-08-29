dcadc3d33025424591375b0044e0c713d31fcd7d
Mihretu <dmercy2015@gmail.com>
2022-08-28 23:38:20 -0700

    task 14

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,21 +1,29 @@
 #include "lists.h"
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: the head address of the linked list
+ * Return: 1 if it's a palindrome, 0 else
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
+	listint_t *cur = *head;
+	int tab[2048], i = 0, j = 0;
 
-	listint_t *temp;
-	if (head == NULL|| *head == NULL)
-		return (1);
-	temp = *head;
-	while (temp) /* get len of list */
+	if (*head)
 	{
-		temp = temp->next;
-		len++;
+		while (cur)
+		{
+			tab[i] = cur->n;
+			cur = cur->next;
+			i++;
+		}
+		while (j < i / 2)
+		{
+			if (tab[j] == tab[i - j - 1])
+				j++;
+			else
+				return (0);
+		}
 	}
-	return (0);
+	return (1);
 }
