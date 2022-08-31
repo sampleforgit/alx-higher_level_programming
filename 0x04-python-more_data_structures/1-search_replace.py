8902718e45eaaac1ae7c444d57a3cb703ddce838
Mihretu <dmercy2015@gmail.com>
2022-08-31 01:50:04 -0700

    task 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    return (sum(set(my_list)))
+def search_replace(my_list, search, replace):
+    return [replace if num == search else num for num in my_list]
