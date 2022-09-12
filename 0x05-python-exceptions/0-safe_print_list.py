fee32a9a2397afe32597c4d08d8fa1add22e22ac
Mihretu <dmercy2015@gmail.com>
2022-09-12 11:10:49 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        while count < x:
+            print("{}".format(my_list[count]), end='')
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
+        pass
