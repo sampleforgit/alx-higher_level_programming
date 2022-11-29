5898fb2dd729bb7e8eaa5cba74e4b475cce26d81
Mihretu <dmercy2015@gmail.com>
2022-11-28 19:06:08 -0800

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints three lines using an array of strings and a loop
+const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < stringArray.length; i++) {
+  console.log(stringArray[i]);
+}
