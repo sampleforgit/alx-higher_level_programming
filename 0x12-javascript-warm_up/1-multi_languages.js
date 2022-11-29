8d975acd32d9a32547575683752be21358bf5ed1
Mihretu <dmercy2015@gmail.com>
2022-11-28 18:56:29 -0800

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints three strings
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'Javascript is amazing';
+console.log(c);
+console.log(python);
+console.log(javascript);
