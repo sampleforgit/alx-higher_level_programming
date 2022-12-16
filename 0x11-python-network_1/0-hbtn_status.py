1bb3af183594a3f2a0aadda75e241eb851c8dcd3
Mihretu <dmercy2015@gmail.com>
2022-12-16 09:26:04 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-''' Script that fetches with a website '''
+"""Fetches the URL: https://intranet.hbtn.io/status
+"""
 
-import urllib.request
+from urllib.request import Request, urlopen
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
 
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+if __name__ == "__main__":
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
